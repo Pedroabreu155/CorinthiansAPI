470e0bc88a9a92e42c1a3c74d0683b8211942b3b
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-05-11 22:13:42 -0300

    new route created, getTopScorers

--- src/routes/corinthians-routes.js
+++ src/routes/corinthians-routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routes = express.Router()
 
-const {
-  getAllChampionships
-} = require('../controllers/championshipsController')
+const {getAllChampionships} = require('../controllers/championshipsController')
+
+const { getAllTopScorers } = require('../controllers/topScorersController')
 
 routes.get("/titulos", getAllChampionships)
+routes.get("/artilheiros", getAllTopScorers)
 
 module.exports = {
   routes: routes
